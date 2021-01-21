a929a960066baf8615a7f3d23dc896bf3255fd82
slido <denis.koletic@gmail.com>
2021-01-21 17:15:28 +0100

    perf(refactor to old state): refactor to old state
    refactor to old state

--- src/components/History.js
+++ src/components/History.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import Status from './Status';
-import Moves from './Moves';
+import React, {useContext, useEffect, useState} from 'react';
+import { GameContext } from '../contexts/GameContext';
 
 function History() {
 
+    const {winner, xIsNext, history, jumpTo } = useContext(GameContext);
+
+    const [status, setStatus] = useState('');
+    const [moves, setMoves ] = useState([]);
+
+    useEffect(() => {
+      setStatus(
+        winner ? `Winner: ${winner}`: `Next player: ${xIsNext ? "X" : "O"}`
+      )
+    },[winner, xIsNext])
+    
+    useEffect(() => {
+      setMoves(history.map((step, move) => {
+        const desc = move ?
+          'Go to move #' + move :
+          'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      }))
+    },[history, jumpTo]);
+// todo status i moves separate components
     return (
           <div>
-            <Status></Status>
-            <Moves></Moves>
+            <div>{status}</div>
+            <ol>{moves}</ol>
           </div>
       );
 }
 
-// napravi git
+
 // kada zavrsim game pokaze se button sa new game, koji izrenda ispod novi game i tako za sljedece
 // kada kliknes button, otvori modal sa pitanjem, dali prvi x, o ili random
 
